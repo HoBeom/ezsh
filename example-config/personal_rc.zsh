3bdf402a61725a6f9f36fe8e0abd29f4f64db107
HoBeom <jhb1365@gmail.com>
2026-03-14 16:20:44 +0900

    Update personal_rc.zsh

--- example-config/personal_rc.zsh
+++ example-config/personal_rc.zsh
@@ -2,7 +2,9 @@
 # Place all your .zshrc configurations (including this one) in a single or multiple files under ~/.config/ezsh/zshrc/ folder
 
 # Additional OH-MY-ZSH plugins to enable
-plugins+=(docker docker-compose pip screen)
+plugins+=(docker docker-compose pip screen zellij conda)
+
+ZSH_ZELLIJ_PREFIX_Z=true
 
 # Enable optional plugins only when the command exists.
 if command -v pyenv >/dev/null 2>&1; then
@@ -32,7 +34,6 @@ fi
 # Custom Aliases, add your personal aliases here
 
 alias p="python"
-alias c="conda"
 alias ww="nvtop"
 alias wd="watch -n 1 du -hs *"
 alias nn="nmon"
@@ -41,8 +42,53 @@ alias tt="gnome-terminal --geometry 185x55"
 alias mux="tmuxinator"
 alias mon="tmuxinator start hwmonitor"
 
+if (( $+commands[exa] )); then
+  alias l='exa --icons -lbF --git'
+  alias ll='exa --icons -lbGF --git'
+  alias llm='exa --icons -lbGd --git --sort=modified'
+  alias la='exa --icons -lbhHigUmuSa --time-style=long-iso --git --color-scale'
+  alias lx='exa --icons -lbhHigUmuSa@ --time-style=long-iso --git --color-scale'
+  alias lS='exa --icons -1'
+  alias lt='exa --icons --tree --level=2'
+fi
+
+SECRETS_DIR="$HOME/.secrets"
+if [[ -d "$SECRETS_DIR" ]]; then
+  for f in "$SECRETS_DIR"/*.zsh(N); do
+    source "$f"
+  done
+fi
+
 export FZF_CTRL_T_OPTS="
   --preview 'bat --style=numbers --color=always --line-range :500 {} 2>/dev/null || 
              tree -C {} | head -200'
   --preview-window=right:60%
 "
+
+if [ -x "$HOME/miniconda3/bin/conda" ]; then
+    __conda_setup="$("$HOME/miniconda3/bin/conda" shell.zsh hook 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__conda_setup"
+    else
+        if [ -f "$HOME/miniconda3/etc/profile.d/conda.sh" ]; then
+            . "$HOME/miniconda3/etc/profile.d/conda.sh"
+        else
+            export PATH="$HOME/miniconda3/bin:$PATH"
+        fi
+    fi
+    unset __conda_setup
+fi
+
+OPENCODE_BIN="$HOME/.opencode/bin"
+if [[ -d "$OPENCODE_BIN" ]]; then
+  path=("$OPENCODE_BIN" $path)
+fi
+
+export BUN_INSTALL="$HOME/.bun"
+if [[ -d "$BUN_INSTALL/bin" ]]; then
+  path=("$BUN_INSTALL/bin" $path)
+fi
+
+if [[ -s "$BUN_INSTALL/_bun" ]]; then
+  source "$BUN_INSTALL/_bun"
+fi
